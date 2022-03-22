685fe59998adf875f0efad07289cfbbbdb8921af
Master Of Attack <harpakddffhh@gmail.com>
2022-03-22 15:21:37 +0430

    test

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCGKYOazg6LRbZ9t2cWBm0jj0JVyu4TZDA",
+    authDomain: "overstore-1.firebaseapp.com",
+    projectId: "overstore-1",
+    storageBucket: "overstore-1.appspot.com",
+    messagingSenderId: "943403186323",
+    appId: "1:943403186323:web:7e32d526572eaf0b173566",
+    measurementId: "G-N7YRKZM81C"
+  };
