3a89b8ada83f8f0be7cdc9610339b41a30f8da1d
Master Of Attack <harpakddffhh@gmail.com>
2022-03-29 15:50:45 +0430

    1:50

--- src/Home.js
+++ src/Home.js
@@ -13,17 +13,20 @@ function home() {
         />
 
         <div className="home__row">
-          <Product />
-          <Product />
+          <Product id= '000001' title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses" price={22.99} image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg'  rating={5} />
+
+          <Product id='000002' title="The Lightning Thief (Percy Jackson and the Olympians, Book 1)" price={5.97} image="https://images-na.ssl-images-amazon.com/images/I/51BfYSYSfxL._SX336_BO1,204,203,200_.jpg" rating={4} />
         </div>
 
         <div className="home__row">
-        <Product />
-        <Product />
-        <Product />
+        <Product id='000003' title="Logitech Brio 4K Webcam, Ultra 4K HD Video Calling, Noise-Canceling mic, HD Auto Light Correction, Wide Field of View, Works with Microsoft Teams, Zoom, Google Voice, PC/Mac/Laptop/Macbook/Tablet" price={169.99} image="https://m.media-amazon.com/images/I/61OQP2+yqML._AC_SL1500_.jpg" rating={4} />
+
+        <Product id='000004' title="Logitech G910 Orion Spectrum RGB Wired Mechanical Gaming Keyboard , Black" price={64.99} image="https://m.media-amazon.com/images/I/61IVHE8oy9L._AC_SL1024_.jpg" rating={4} />
+
+        <Product id='000005' title="Razer BlackShark V2 Gaming Headset: THX 7.1 Spatial Surround Sound - 50mm Drivers - Detachable Mic - PC, PS4, PS5, Switch, Xbox One, Xbox Series X and S, Mobile - 3.5 mm Audio Jack and USB DAC - Black" price={99.99} image="https://m.media-amazon.com/images/I/61wrOi+03FL._AC_SL1500_.jpg" rating={3} />
         </div>
         <div className="home__row">
-        <Product />
+        <Product id='000006' title="SAMSUNG 85-Inch Class Neo QLED 8K QN800A Series - 8K UHD Quantum HDR 32x Smart TV with Alexa Built-in (QN85QN800AFXZA, 2021 Model)" price="3,400.99" image="https://m.media-amazon.com/images/I/81bmOxFZYWL._AC_SL1500_.jpg" rating={3} />
         </div>
       </div>
     </div>
--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import './Product.css';
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
       
     <div className='product'>
         <div className="product__info">
-            <p>The lean startup</p>
-            <p className='product__price'><small>$</small>
-            <strong>19.99</strong>
-            </p>
+            <span className='product__title'>{title}</span>
+            <span className='product__price'><small>$</small>
+            <strong>{price}</strong>
+            </span>
             <div className="product__rating">
-                <p>⭐</p>
+                {Array(rating).fill().map((_, i) => (
+                    <p>⭐</p>
+                ))}
+                
             </div>
         </div>
-        {/* eslint-disable-next-line jsx-a11y/alt-text */}
-        <img src='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg' />
+        <img src={image} alt="" />
 
         <button>Add to Basket</button>
     </div>
